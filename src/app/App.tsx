d90aaaa5156daed4a79f89843afa22d084a900c7
Yuri <player2905@gmail.com>
2023-08-20 16:39:07 +0300

    added modal

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,30 @@
-import './styles/index.scss'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { AppRouter } from 'app/router'
 import { Navbar } from 'widgets/Navbar'
 import { Sidebar } from 'widgets/Sidebar'
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
+import { Modal } from 'shared/ui/Modal/Modal'
 import { useTheme } from './providers/ThemeProvider'
+import { type } from 'os'
 
 const App = () => {
     const { theme } = useTheme()
+
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+                <button onClick={() => setIsOpen(true)} type="button">
+                    toggle
+                </button>
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro illum quos
+                    debitis recusandae modi aperiam, facere iusto delectus commodi assumenda,
+                    perferendis aliquid exercitationem atque fugiat, voluptatem architecto
+                    praesentium ipsum culpa?
+                </Modal>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />

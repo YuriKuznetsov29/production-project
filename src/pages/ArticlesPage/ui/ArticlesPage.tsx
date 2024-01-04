f4ca53dc7c2aba751c7e7b870b6c57334dd32567
Yuri Kuznetsov <playeer2905@gmail.com>
2024-01-04 23:22:09 +0300

    add fetching articles, view type selector

--- src/pages/ArticlesPage/ui/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage.tsx
@@ -1,49 +1,61 @@
 import { classNames } from 'shared/lib/classNames/classNames'
-import { memo } from 'react'
-import { Article, ArticleList } from 'entities/Article'
+import { memo, useCallback } from 'react'
+import { Article, ArticleList, ArticleView, ArticleViewSelector } from 'entities/Article'
+import {
+    DynamicModuleLoader,
+    ReducerList,
+} from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader'
+import { useAppDispatch } from 'shared/lib/hook/useAppDispatch/useAppDispatch'
+import { useInitialEffect } from 'shared/lib/hook/useInitialEffect/useInitialEffect'
+import { useSelector } from 'react-redux'
+import {
+    articlesPageActions,
+    articlesPageReducer,
+    getArticles,
+} from '../model/slices/articlePageSlice'
+import { fetchArticlesList } from '../model/services/fetchArticlesList/fetchArticlesList'
 import cls from './ArticlesPage.module.scss'
+import {
+    getArticlesPageError,
+    getArticlesPageIsLoading,
+    getArticlesPageView,
+} from '../model/selectors/articles.PageSelectors'
 
 interface ArticlesPageProps {
     className?: string
 }
 
-const article = {
-    id: '2',
-    title: 'Python news',
-    subtitle: 'Что нового в JS за 2022 год?',
-    img: 'https://zsfond.ru/wp-content/uploads/2021/03/piton-1-1024x578.jpg',
-    views: 1022,
-    createdAt: '26.02.2022',
-    user: {
-        id: '1',
-        username: '123',
-        avatar: 'https://teknotower.com/wp-content/uploads/2020/11/js.png',
-    },
-    userId: '1',
-    type: ['IT'],
-    blocks: [
-        {
-            id: '1',
-            type: 'TEXT',
-            title: 'Заголовок этого блока',
-            paragraphs: [
-                'Программа, которую по традиции называют «Hello, world!», очень проста. Она выводит куда-либо фразу «Hello, world!», или другую подобную, средствами некоего языка.',
-                'JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.',
-                'Существуют и другие способы запуска JS-кода в браузере. Так, если говорить об обычном использовании программ на JavaScript, они загружаются в браузер для обеспечения работы веб-страниц. Как правило, код оформляют в виде отдельных файлов с расширением .js, которые подключают к веб-страницам, но программный код можно включать и непосредственно в код страницы. Всё это делается с помощью тега <script>. Когда браузер обнаруживает такой код, он выполняет его. Подробности о теге script можно посмотреть на сайте w3school.com. В частности, рассмотрим пример, демонстрирующий работу с веб-страницей средствами JavaScript, приведённый на этом ресурсе. Этот пример можно запустить и средствами данного ресурса (ищите кнопку Try it Yourself), но мы поступим немного иначе. А именно, создадим в каком-нибудь текстовом редакторе (например — в VS Code или в Notepad++) новый файл, который назовём hello.html, и добавим в него следующий код:',
-            ],
-        },
-    ],
-} as Article
+const resucers: ReducerList = {
+    articlesPage: articlesPageReducer,
+}
 
 const ArticlesPage = ({ className }: ArticlesPageProps) => {
+    const dispatch = useAppDispatch()
+
+    const articles = useSelector(getArticles.selectAll)
+    const isLoading = useSelector(getArticlesPageIsLoading)
+    const error = useSelector(getArticlesPageError)
+    const view = useSelector(getArticlesPageView)
+
+    const onChangeView = useCallback(
+        (view: ArticleView) => {
+            dispatch(articlesPageActions.setView(view))
+        },
+        [dispatch]
+    )
+
+    useInitialEffect(() => {
+        dispatch(fetchArticlesList)
+        dispatch(articlesPageActions.initState)
+    })
+
     return (
-        <div className={classNames(cls.ArticlesPage, {}, [className])}>
-            <ArticleList
-                articles={new Array(16)
-                    .fill(0)
-                    .map((item, index) => ({ ...article, id: String(item) }))}
-            />
-        </div>
+        <DynamicModuleLoader reducers={resucers}>
+            <div className={classNames(cls.ArticlesPage, {}, [className])}>
+                <ArticleViewSelector view={view} onViewClick={onChangeView} />
+                <ArticleList isLoading={isLoading} view={view} articles={articles} />
+            </div>
+        </DynamicModuleLoader>
     )
 }
 

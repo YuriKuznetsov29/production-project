8e46ba8f052930b0b6450ed82b3a70aec409c8ee
Yuri Kuznetsov <playeer2905@gmail.com>
2024-01-11 21:58:24 +0300

    add fetchNextArticlesPage test

--- src/pages/ArticlesPage/model/services/fetchNextArticlesPage/fetchNextArticlesPage.test.ts
+++ src/pages/ArticlesPage/model/services/fetchNextArticlesPage/fetchNextArticlesPage.test.ts
@@ -3,23 +3,44 @@ import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk'
 import { Country } from 'entities/Country'
 import { Currency } from 'entities/Currency'
 import { fetchNextArticlesPage } from './fetchNextArticlesPage'
+import { fetchArticlesList } from '../fetchArticlesList/fetchArticlesList'
 
-jest.mock('axios')
-
-const mockedAxios = jest.mocked(axios, true)
-
-const data = {
-    username: 'admin',
-    age: 29,
-    country: Country.Russia,
-    lastname: '123',
-    first: 'asd',
-    city: 'asf',
-    currency: Currency.USD,
-}
+jest.mock('../fetchArticlesList/fetchArticlesList')
 
 describe('fetchNextArticlesPage.test', () => {
     test('success', async () => {
-        const thunk = new TestAsyncThunk(fetchNextArticlesPage, { articlesPage: {} })
+        const thunk = new TestAsyncThunk(fetchNextArticlesPage, {
+            articlesPage: {
+                ids: [],
+                page: 2,
+                entities: {},
+                limit: 5,
+                isLoading: false,
+                hasMore: true,
+            },
+        })
+
+        await thunk.callThunk()
+
+        expect(thunk.dispatch).toBeCalledTimes(4)
+        expect(thunk.dispatch).toHaveBeenCalledWith({ page: 3 })
+    })
+
+    test('not called', async () => {
+        const thunk = new TestAsyncThunk(fetchNextArticlesPage, {
+            articlesPage: {
+                ids: [],
+                page: 2,
+                entities: {},
+                limit: 5,
+                isLoading: false,
+                hasMore: false,
+            },
+        })
+
+        await thunk.callThunk()
+
+        expect(thunk.dispatch).toBeCalledTimes(2)
+        expect(thunk.dispatch).not.toHaveBeenCalled()
     })
 })

731848ea3e69f6cfdbb3c74b8b5a8c33617f7bd8
Yuri Kuznetsov <playeer2905@gmail.com>
2024-02-24 16:59:55 +0300

    add avatar with menu, update navbar

--- src/shared/ui/Dropdown/Dropdown.tsx
+++ src/shared/ui/Dropdown/Dropdown.tsx
@@ -2,6 +2,8 @@ import { Menu } from '@headlessui/react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { Fragment, ReactNode } from 'react'
 import cls from './Dropdown.module.scss'
+import { DropdownDirection } from 'shared/types/ui'
+import { AppLink } from '../AppLink/AppLink'
 
 export interface DropdownItem {
     disabled?: boolean
@@ -14,27 +16,48 @@ interface DropdownProps {
     className: string
     items: DropdownItem[]
     trigger: ReactNode
+    direction?: DropdownDirection
+}
+
+const mapDirectionClass: Record<DropdownDirection, string> = {
+    'bottom left': cls.optionsBottomLeft,
+    'bottom right': cls.optionsBottomRight,
+    'top right': cls.optionsTopRight,
+    'top left': cls.optionsTopLeft,
 }
 
 export function Dropdown(props: DropdownProps) {
-    const { className, items, trigger } = props
+    const { className, items, trigger, direction = 'bottom right' } = props
+
+    const menuClasses = [mapDirectionClass[direction]]
 
     return (
         <Menu as="div" className={classNames(cls.Dropdown, {}, [className])}>
             <Menu.Button className={cls.btn}>{trigger}</Menu.Button>
-            <Menu.Items className={cls.menu}>
+            <Menu.Items className={classNames(cls.menu, {}, menuClasses)}>
                 {items.map((item) => {
+                    const content = ({ active }: { active: boolean }) => (
+                        <button
+                            className={classNames(cls.item, { [cls.active]: active })}
+                            onClick={item.onClick}
+                            type="button"
+                            disabled={item.disabled}
+                        >
+                            {item.content}
+                        </button>
+                    )
+
+                    if (item.href) {
+                        return (
+                            <Menu.Item as={AppLink} to={item.href} disabled={item.disabled}>
+                                {content}
+                            </Menu.Item>
+                        )
+                    }
+
                     return (
                         <Menu.Item as={Fragment} disabled={item.disabled}>
-                            {({ active }) => (
-                                <button
-                                    className={classNames(cls.item, { [cls.active]: active })}
-                                    onClick={item.onClick}
-                                    type="button"
-                                >
-                                    {item.content}
-                                </button>
-                            )}
+                            {content}
                         </Menu.Item>
                     )
                 })}
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -9,6 +9,8 @@ import { Text, TextTheme } from 'shared/ui/Text/Text'
 import cls from './Navbar.module.scss'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
 import { RoutePath } from 'shared/config/routeConfig/routeConfig'
+import { Dropdown } from 'shared/ui/Dropdown/Dropdown'
+import { Avatar } from 'shared/ui/Avatar/Avatar'
 
 interface NavbarProps {
     className?: string
@@ -41,9 +43,22 @@ export const Navbar = memo(({ className }: NavbarProps) => {
                     {' '}
                     {t('Создать статью')}{' '}
                 </AppLink>
-                <Button className={cls.links} theme={ButtonTheme.CLEAR_INVERTED} onClick={onLogout}>
-                    {t('Выйти')}
-                </Button>
+                <Dropdown
+                    className={cls.dropdown}
+                    items={[
+                        {
+                            content: t('Профиль'),
+                            onClick: onLogout,
+                            href: RoutePath.profile + authData.id,
+                        },
+                        {
+                            content: t('Выйти'),
+                            onClick: onLogout,
+                        },
+                    ]}
+                    trigger={<Avatar size={30} src={authData.avatar} />}
+                    direction="bottom left"
+                />
             </header>
         )
     }
